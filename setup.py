9c7d7afbf03187b06954d7eb7615b30aba53b5dd
Andrey M <meamka@ya.ru>
2016-03-20 04:08:50 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Meadow',
+    version='0.1',
+    packages=['meadow'],
+    url='https://github.com/amka/meadow/',
+    license='BSD',
+    author='Andrey Maksimov',
+    author_email='meamka@ya.ru',
+    description='Python virtual file system sample app',
+    long_description=__doc__,
+    install_requires=[
+        'flask==0.10.1',
+    ],
+)
